9551f7dea489c51c5cc6d116d8e9dd7b896b3097
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-16 18:38:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,9 +4,3 @@ provider "google" {
   project     = var.project_id                   # GCP Project ID
   region      = var.region                       # Region to deploy the resources
 }
-
-provider "google-beta" {
-  credentials = file(var.credentials_file_path)
-  project     = var.project_id
-  region      = var.region
-}
