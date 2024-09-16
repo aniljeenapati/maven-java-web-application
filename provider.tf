4ea8528bb0ecc566f3a5e17d40e01f91d29d8000
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-16 18:36:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # provider.tf
 provider "google" {
-  credentials = file(var.credentials_file_path)  # Path to your GCP service account JSON key
+ # credentials = file(var.credentials_file_path)  # Path to your GCP service account JSON key
   project     = var.project_id                   # GCP Project ID
   region      = var.region                       # Region to deploy the resources
 }
