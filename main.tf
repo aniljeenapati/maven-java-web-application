24ea31412e2624e7ac7a192d5a3702937e0a5948
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-19 17:05:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,65 +1,38 @@
-# main.tf
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
+  initial_node_count = var.node_count
+
   node_config {
-    machine_type = var.machine_type
-    oauth_scopes  = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    machine_type = var.node_machine_type
   }
 
-  initial_node_count = 1
+  remove_default_node_pool = true
+  min_master_version       = "1.21.9-gke.2100"  // Adjust as needed
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
+  name       = "${google_container_cluster.primary.name}-node-pool"
   location   = var.region
   cluster    = google_container_cluster.primary.name
+  node_count = var.node_count
+
   node_config {
-    machine_type = var.machine_type
-    disk_size_gb = var.disk_size_gb
-    oauth_scopes  = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-  initial_node_count = 1
-}
-
-resource "google_compute_instance_group_manager" "node_pool_mig" {
-  name               = "${var.cluster_name}-mig"
-  zone               = var.zone
-  version {
-    instance_template = google_compute_instance_template.instance_template.id
-  }
-  base_instance_name = "${var.cluster_name}-instance"
-  target_size        = var.target_size
-}
-
-resource "google_compute_instance_template" "instance_template" {
-  name_prefix = "${var.cluster_name}-template"
-  machine_type = var.machine_type
-  disk {
-    auto_delete = true
-    boot        = true
-    source_image = var.image  # Boot disk image
-  }
-  network_interface {
-    network = "default"
-    access_config {
-      // Include this block to create an external IP address
-    }
+    machine_type = var.node_machine_type
   }
 }
+output "kubeconfig" {
+  description = "The kubeconfig file contents"
+  value       = google_container_cluster.primary.kube_config_raw
+}
 
-resource "google_compute_autoscaler" "autoscaler" {
-  name   = "${var.cluster_name}-autoscaler"
-  zone   = var.zone
-  target = google_compute_instance_group_manager.node_pool_mig.id
+output "cluster_name" {
+  description = "The name of the Kubernetes cluster"
+  value       = google_container_cluster.primary.name
+}
 
-  autoscaling_policy {
-    min_replicas = var.min_replicas
-    max_replicas = var.max_replicas
-  }
+output "cluster_endpoint" {
+  description = "The endpoint of the Kubernetes cluster"
+  value       = google_container_cluster.primary.endpoint
 }
