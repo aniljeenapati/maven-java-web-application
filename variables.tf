8b7d1cb9bf6bcce185f0ee41a3b8cc982dfdffa0
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-19 17:02:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,57 +1,28 @@
-# variables.tf
-variable "credentials_file_path" {
-  description = "Path to the GCP service account key file"
-  type        = string
-}
-
 variable "project_id" {
-  description = "GCP project ID"
+  description = "The GCP project ID"
   type        = string
 }
 
 variable "region" {
-  description = "GCP region"
+  description = "The region where the resources will be created"
   type        = string
-}
-
-variable "zone" {
-  description = "GCP zone"
-  type        = string
+  default     = "us-central1"
 }
 
 variable "cluster_name" {
-  description = "Name of the Kubernetes cluster"
+  description = "The name of the Kubernetes cluster"
   type        = string
+  default     = "my-cluster"
 }
 
-variable "machine_type" {
-  description = "Machine type for the nodes"
+variable "node_count" {
+  description = "The number of nodes in the Kubernetes cluster"
+  type        = number
+  default     = 2
+}
+
+variable "node_machine_type" {
+  description = "The type of machine to use for nodes in the Kubernetes cluster"
   type        = string
-  default     = "n1-standard-1"
-}
-
-variable "disk_size_gb" {
-  description = "Disk size for the VM instances"
-  type        = number
-  default     = 100
-}
-
-variable "image" {
-  description = "Image to use for the instance template"
-  type        = string
-}
-
-variable "min_replicas" {
-  description = "Minimum number of replicas"
-  type        = number
-}
-
-variable "max_replicas" {
-  description = "Maximum number of replicas"
-  type        = number
-}
-
-variable "target_size" {
-  description = "Target size of the instance group"
-  type        = number
+  default     = "e2-medium"
 }
