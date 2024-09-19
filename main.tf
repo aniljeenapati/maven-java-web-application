ec796b59613684cdec10bafda384c2c58f2f4ef5
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-19 17:44:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  remove_default_node_pool = true
+  remove_default_node_pool = true  // We're removing the default node pool
   min_master_version       = "1.21.9-gke.2100"  // Adjust as needed
 
   node_config {
@@ -14,7 +14,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "${google_container_cluster.primary.name}-node-pool"
   location   = var.region
   cluster    = google_container_cluster.primary.name
-  node_count = var.node_count
+  node_count = var.node_count  // Initial node count is defined here, in the custom node pool
 
   node_config {
     machine_type = var.node_machine_type
