75a73b3e58d535855cfc7de39ffdb1fd2e60cf50
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-19 17:35:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
-  default     = "kube-435112"
+  default     = "kube-435112"  
 }
 
 variable "region" {
   description = "The region where the resources will be created"
   type        = string
-  default     = "us-central1"
+  default     = "us-central1"  
 }
 
 variable "cluster_name" {
   description = "The name of the Kubernetes cluster"
   type        = string
-  default     = "my-cluster"
+  default     = "my-cluster" 
 }
 
 variable "node_count" {
   description = "The number of nodes in the Kubernetes cluster"
   type        = number
-  default     = 2
+  default     = 2  
 }
 
 variable "node_machine_type" {
   description = "The type of machine to use for nodes in the Kubernetes cluster"
   type        = string
-  default     = "e2-medium"
+  default     = "e2-medium"  // Adjust based on the required machine type
 }
